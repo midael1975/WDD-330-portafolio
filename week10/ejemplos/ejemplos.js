aae42c05aaf51252ecd9fc752f2efa63bfce5b21
midael1975 <wjpd1905@gmail.com>
2021-06-24 15:36:43 -0400

    init commit

--- week10/ejemplos/ejemplos.js
+++ week10/ejemplos/ejemplos.js
@@ -7,3 +7,33 @@ email.addEventListener("input", function(event) {
         email.setCustomValidity("Thanks, your email is correct");
     }
 });
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+const ul = document.getElementById('authors');
+const url = 'https://randomuser.me/api/?results=10';
+
+fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+        let authors = data.results;
+        return authors.map(function(author) {
+            let li = createNode('li');
+            let img = createNode('img');
+            let span = createNode('span');
+            img.src = author.picture.medium;
+            span.innerHTML = `${author.name.first} ${author.name.last}`;
+            append(li, img);
+            append(li, span);
+            append(ul, li);
+        })
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
