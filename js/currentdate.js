9775fff2e5347676dbe9b9b3259e2092c49551d5
midael1975 <wjpd1905@gmail.com>
2021-07-15 10:41:42 -0400

    init commit

---
+++ js/currentdate.js
@@ -0,0 +1,10 @@
+const  options  =   {  weekday:   'long',  day:   'numeric',  month:   'long',  year:   'numeric',  hour:   'numeric',  minute:   'numeric',  second: 'numeric', timezone: 'long'  };
+document.getElementById('currentdate').textContent  =  new  Date().toLocaleDateString('en-us',  options);
+
+var d = new Date();
+document.getElementById("fullYear").innerHTML = d.getFullYear();
+
+function lastModified() {
+    Document.write("Last Modified:" + document.lastModified);
+}
+lastModified();
